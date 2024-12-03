0fc3622a402e5d3aab8cf38fbb23b1b0b59527a0
yalisa4 <yalisasintiago@gmail.com>
2024-12-03 15:52:06 +0100

    Completed part 1

--- 2024/AoC_2024_2.py
+++ 2024/AoC_2024_2.py
@@ -1,23 +1,29 @@
+def safe_check(record):
+  for number in range(0, len(record)-1, 1):
+    if abs(record[number] - record[number + 1]) > 3 or abs(record[number] - record[number + 1]) < 1:
+      return False
+    
+  if sorted(record) not in [record, record[::-1]]:
+    return False
+  
+  else:
+    return True
+      
+
 def red_nosed(datastream: list[list: int]) -> int:
   safe_count = 0
   for record in datastream:
-    safe = True
-    for index in range(0, len(record) - 1):
-      if abs(record[index] - record[index + 1]) > 3:
-        safe = False
-        break
-    
-    if all(record[i] <= record[i + 1] for i in range(len(record) - 1)) or all(record[i] >= record[i + 1] for i in range(len(record) - 1)) and safe:
-        safe_count += 1
+    safe_count += 1 if safe_check(record) else 0
 
   return safe_count
+
 
 if __name__ == "__main__":
   with open("2024/AoC_2024_2.txt") as fh:
     content = fh.readlines()
   
-  list = [line.split(" ") for line in content]
-  cleaned_list = [[item.strip() for item in sublist] for sublist in list]
+  list_ = [line.split(" ") for line in content]
+  cleaned_list = [[item.strip() for item in sublist] for sublist in list_]
   list_int = [[int(num) for num in sublist] for sublist in cleaned_list]
 
   print(red_nosed(list_int))
