8064677f4f819d7cc50db3703d80a8134e44f84d
yalis4 <yalisasintiago@gmail.com>
2025-05-05 22:49:35 +0200

    finished part 2

--- 2015/2015_day3.py
+++ 2015/2015_day3.py
@@ -18,16 +18,31 @@ def aoc_3(content):
         "v": down_dir
     }
 
+    # part 1
     current_pos = (0,0)
     visited = set()
     visited.add(current_pos)
 
-    for char in content:
-        if char in commando:
-            current_pos = commando[char](current_pos)
-            visited.add(current_pos)
-    
-    return len(visited)
+    # part 2
+    current_pos_santa, current_pos_robot = (0,0), (0,0)
+    visited_santa_robot = set()
+    visited_santa_robot.add(current_pos)
+
+    for index, char in enumerate(content):
+        if char not in commando:
+            continue
+
+        current_pos = commando[char](current_pos)
+        visited.add(current_pos)
+
+        if index % 2 == 0:
+            current_pos_santa = commando[char](current_pos_santa)
+            visited_santa_robot.add(current_pos_santa)
+        else:
+            current_pos_robot = commando[char](current_pos_robot)
+            visited_santa_robot.add(current_pos_robot)
+
+    return len(visited), len(visited_santa_robot)
 
 if __name__ == "__main__":
     with open("2015/2015_day3.txt") as file:
